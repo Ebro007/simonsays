39b4802ad722977e3c274d63e490b93a5f492cca
Ebro007 <ebrokramer@yahoo.com>
2018-07-24 13:17:59 -0500

    Extra

--- scripts.sh/get_user_input.py
+++
@@ -1,9 +0,0 @@
-from getpass import getpass
-
-print "What is your Name?"
-name = raw_input()
-
-color = getpass('What is your favorite color?')
-
-print "Hi", name
-print "Your favorite color is:", color
