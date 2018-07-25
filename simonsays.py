31700215856938c768981569e63699dd7b8ccecd
Ebro007 <ebrokramer@yahoo.com>
2018-07-25 09:42:39 -0500

    final commit for unfinished version

--- simonsays.py
+++ simonsays.py
@@ -1,41 +1,42 @@
-import time
-import random
 import RPi.GPIO as GPIO
 import LEDRGB as LED
-
-colors = ['R', 'G', 'B', 'Y']
+import time
+import random
+# this script appends a value to a list.
 R_pin = 11
 G_pin = 12
 B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-
 buzz_pin = 32
 GPIO.setup(buzz_pin, GPIO.OUT)
-buzz = GPIO.PWM(buzz_pin, 1000)
+Buzz = GPIO.PWM(buzz_pin,1000)
 
-
-frequencies= [250, 500, 750, 1500]
-
+colors = ['R', 'G', 'B', 'Y']
+frequencies = [220, 440, 880, 1760]
 
 def loop():
-	while True:
-		n = random.randint(0,3)
-		LED.setColor(colors[n])
-		buzz.ChangeFrequency(frequencies[n])
-		buzz.start(50)
-		time.sleep(0.5)
-		buzz.stop
-		time.sleep(0.5)
-		LED.noColor()
-		time.sleep(0.5)
+    n = random.randint(0,3)
+    color_sequence = [colors[n]]
+    frequency_sequence = [frequencies[n]]
+    while True:
+        for i in range(0, len(color_sequence)):
+            Buzz.ChangeFrequency(frequency_sequence[i])
+            Buzz.start(50)
+            LED.setColor(color_sequence[i])
+            time.sleep(0.5)
+            Buzz.stop()
+            LED.noColor()
+            time.sleep(0.5)
+        n = random.randint(0,3)
+        color_sequence.append(colors[n])
+        frequency_sequence.append(frequencies[n])
+        time.sleep(0.5)
 
-if __name__=='__main__':
-	try:
-		loop()
-	except KeyboardInterrupt:
-		print 'Bye!'
-		LED.destroy()
-
+if __name__ == '__main__':
+    try:
+        loop()
+    except KeyboardInterrupt:
+        print "Good Bye"
+LED.destroy()
